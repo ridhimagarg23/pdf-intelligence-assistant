44f5fe56fbbc29587d234d3d0f0df33ccacba0e1
Ridhima Garg <ridhimagarg2301@gmail.com>
2026-06-03 19:57:10 +0530

    Disable OCR for deployment

--- src/parser.py
+++ src/parser.py
@@ -1,8 +1,18 @@
 from docling.document_converter import DocumentConverter
+from docling.datamodel.pipeline_options import PdfPipelineOptions
+
 
 def extract_document(pdf_path):
 
-    converter = DocumentConverter()
+    pipeline_options = PdfPipelineOptions()
+
+    # Temporary: disable OCR for Streamlit deployment
+    pipeline_options.do_ocr = False
+
+    converter = DocumentConverter(
+        pipeline_options=pipeline_options
+    )
+
     result = converter.convert(pdf_path)
 
-    return result.document
+    return result.document.export_to_markdown()
